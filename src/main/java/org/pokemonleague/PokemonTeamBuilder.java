da4e2203812165fd3eca68f1f862072385554263
Darigan, David <darigan@protonmail.com>
2023-04-14 14:43:40 +0100

    Initial commit

---
+++ src/main/java/org/pokemonleague/PokemonTeamBuilder.java
@@ -0,0 +1,11 @@
+package org.pokemonleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonTeamBuilder {
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonTeamBuilder.class, args);
+    }
+}
